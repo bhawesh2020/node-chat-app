ce3c80e8bd0121cce0ab11255161607dc00ff532
bhawesh2020 <bhawesh2020@gmail.com>
2018-12-06 09:57:44 +0530

    Add newMessage and createMessage event

---
+++ public/js/index.js
@@ -0,0 +1,24 @@
+var socket=io();
+socket.on('connect',function () {
+    console.log('connected to server');
+    // socket.emit('createEmail',{
+    //     to:'qwerty@example.com',
+    //     text:'random'
+    // });
+    socket.emit('createMessage',{
+        to:'qwerty@example.com',
+        text:'random'
+    });
+});
+
+socket.on('disconnect',function(){
+    console.log('disconnected from server');
+});
+
+// socket.on('newEmail',function (data) {
+//     console.log('new email',data);
+// });
+
+socket.on('newMessage',function (data) {
+    console.log('new message',data);
+});
--- server/server.js
+++ server/server.js
@@ -13,6 +13,27 @@ app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
     console.log(`new user connected`);
+
+    // socket.emit('newEmail',{
+    //     from:'someone@qwerty.com',
+    //     createdAt:12345,
+    //     text:'Hello world'
+    // });
+
+    socket.emit('newMessage',{
+        from:'someone@qwerty.com',
+        createdAt:12345,
+        text:'Hello world'
+    });
+
+    // socket.on('createEmail',(data)=>{
+    //     console.log('create email',data);
+    // });
+
+    socket.on('createMessage',(data)=>{
+        console.log('create message',data);
+    });
+
     socket.on('disconnect',()=>{
         console.log('User disconnected');
     });
